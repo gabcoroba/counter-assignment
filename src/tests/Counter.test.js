f66f89c71cab2753ae06aab92d64af973fb17cc0
gabcoroba <gabcoroba@gmail.com>
2023-11-17 16:38:10 -0500

    Revert "adding counter component tests"
    This reverts commit e3a880ba56c2e97d8faa34fee79d1820e9a79a1c.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,41 +1,22 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import Counter from '../components/Counter';
+// import necessary react testing library helpers here
+// import the Counter component here
 
 beforeEach(() => {
   // Render the Counter component here
-  render(<Counter />);
 })
 
 test('renders counter message', () => {
   // Complete the unit test below based on the objective in the line above
-  const counterMessage = screen.getByText(/counter/i);
-  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
   // Complete the unit test below based on the objective in the line above
-  const initial = screen.getByTestId('count');
-  expect(initial).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const incrementButton = screen.getByText('+');
-
-  const count = screen.getByTestId('count');
-
-  fireEvent.click(incrementButton);
-
-  expect(count).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
   // Complete the unit test below based on the objective in the line above
-  const decrementButton = screen.getByText('-');
-
-  const count = screen.getByTestId('count');
-
-  fireEvent.click(decrementButton);
-
-  expect(count).toHaveTextContent('1');
 });
